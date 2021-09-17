af7de1ab3fc8ad9584db4e8c1024e590bc8d344d
codes <https://github.com/CODESQUAREL/badind.git>
2021-09-17 17:35:42 +0900

    9/17 한글화

--- BADIND/asgi.py
+++ BADIND/asgi.py
@@ -1,5 +1,5 @@
 """
-ASGI config for July2_강의보고따라하기 project.
+ASGI config for BADIND project.
 
 It exposes the ASGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,6 @@ import os
 
 from django.core.asgi import get_asgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'July2_강의보고따라하기.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'BADIND.settings')
 
 application = get_asgi_application()
--- BADIND/urls.py
+++ BADIND/urls.py
@@ -1,4 +1,4 @@
-"""July2_강의보고따라하기 URL Configuration
+"""BADIND URL Configuration
 
 The `urlpatterns` list routes URLs to views. For more information please see:
     https://docs.djangoproject.com/en/3.2/topics/http/urls/
